bd32790df188f39f21d920aefe144573f7547ba0
mahusama <mahnoorahmed182@gmail.com>
2026-02-16 01:30:27 +0000

    hero

--- components/IntroSection.tsx
+++ components/IntroSection.tsx
@@ -13,27 +13,28 @@ export default function Intro() {
       <div className="max-w-6xl w-full py-16 md:py-32 text-center md:text-left">
         
         <motion.h1
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white"
-        >
-          MAHNOOR AHMED
-        </motion.h1>
+  initial={{ opacity: 0, y: 20 }}
+  animate={{ opacity: 1, y: 0 }}
+  className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white"
+>
+  MAHNOOR AHMED
+</motion.h1>
 
-        <p className="mt-4 text-lg sm:text-xl md:text-2xl text-white">
-          Full-Stack Software Engineer & Data Engineer
-        </p>
+<p className="mt-4 text-lg sm:text-xl md:text-2xl text-gray-300">
+  Full-Stack Engineer & Data Engineer
+</p>
 
-        <p className="mt-6 max-w-3xl mx-auto md:mx-0 text-base sm:text-lg text-white leading-relaxed">
-          I have 5+ years building scalable healthcare systems, analytics platforms,
-          and data pipelines. I am passionate about creating efficient, user-friendly
-          applications that solve real-world problems and drive business success.
-          I love looking at problems and finding the most efficient way to solve them.
-          I am always looking for new challenges and opportunities to learn and grow
-          as a developer.
-        </p>
+    <p className="mt-6 max-w-2xl mx-auto md:mx-0 text-base sm:text-lg text-gray-200 leading-relaxed">
+    Engineering scalable healthcare systems and data platforms that transform operational workflows and drive measurable business growth.
+    </p>
 
         <div className="flex justify-center md:justify-start mt-8">
+          <a href="#projects">
+      <Button>
+        View Projects
+      </Button>
+          </a>
+
           <a href="/Mahnoor_Ahmed.pdf" target="_blank" rel="noopener noreferrer">
             <Button variant="outline">
               Download Resume
